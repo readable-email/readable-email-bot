8c92c3695fa2999d09d848d2318e253ea9417bc8
Forbes Lindesay <forbes@lindesay.co.uk>
2014-07-08 21:30:00 +0100

    Make it automatically pick up on the lists to check

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 readable-lists-bot
 
-[![Build Status](https://travis-ci.org/readable-lists/readable-lists-bot.png?branch=master)](https://travis-ci.org/readable-lists/readable-lists-bot)
-[![Dependency Status](https://gemnasium.com/readable-lists/readable-lists-bot.png)](https://gemnasium.com/readable-lists/readable-lists-bot)
-[![NPM version](https://badge.fury.io/js/readable-lists-bot.png)](http://badge.fury.io/js/readable-lists-bot)
-
 ## Installation
 
     npm install readable-lists-bot
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-'use strict'
+'use strict';
+
+var connect = require('readable-lists-api');
+var run = require('./lib/run-bot.js');
+
+var readers = {};
+readers.pipermail = require('./lib/readers/pipermail');
+
+var db = connect(process.env.DATABASE);
+
+function onList(list) {
+  console.log(list.source);
+}
+function onError(err) {
+  console.error(err.stack || err);
+}
+function runBot() {
+  db.getLists().then(function (lists) {
+    function next() {
+      if (lists.length === 0) return;
+      var list = lists.pop();
+      onList(list);
+      var source = new readers[list.type](list._id, list.source);
+      return run(source, db).then(null, onError).then(next);
+    }
+    return next();
+  }).then(null, onError).done(runBot);
+}
+runBot();
